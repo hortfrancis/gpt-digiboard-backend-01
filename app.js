1107214764141fc586f62089c6d481f93b4002c8
hortfrancis <ajhf@hotmail.co.uk>
2024-04-15 16:36:00 +0100

    Add POST endpoint for message data

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Server available: gpt-digiboard-backend-01');
+});
+
+app.post('/message', (req, res) => {
+    console.log('req.body:', req.body);
+    console.log('req.body.message:', req.body.message);
+    res.send('Message received');
 });
 
 module.exports = app;
